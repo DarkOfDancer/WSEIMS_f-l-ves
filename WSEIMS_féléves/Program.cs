978cedfacd7919e66c823601795719c6ab7fbef5
zsofi <zsofi@DESKTOP-AB1QDCC>
2024-05-03 12:46:20 +0200

    Add project files.

---
+++ WSEIMS_féléves/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSEIMS_féléves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
